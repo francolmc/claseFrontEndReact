8df4b1af539fb0b8922b86e3f1ed46c3d6b775b7
francolmc <franco.morales@outlook.com>
2022-07-18 22:41:23 -0400

    feat: ejemplo axios

--- src/pages/sample/sample.jsx
+++ src/pages/sample/sample.jsx
@@ -1,8 +1,32 @@
+import axios from "axios";
 import React, { useState } from "react";
 
 const Sample = () => {
     const [ contador, setContador ] = useState(0);
     const [userName, setUserName] = useState('');
+    const [posts, setPosts] = useState([]);
+    const [ postTile, setPostTile ] = useState('');
+    const [ postContent, setPostContent ] = useState('');
+
+    const getPosts = async () => {
+        const postsFromAPI = await axios.get('https://backend-posts-test.herokuapp.com/api/post', {
+            headers: {
+                'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImZyYW5jby5tb3JhbGVzQG91dGxvb2suY29tIiwic3ViIjo0LCJpYXQiOjE2NTgxOTc1ODksImV4cCI6MTY1ODIwMTE4OX0.wKyxRQJcqxVDfbjO_U4EsvYfOc2lX-zuij6bFc46MjM'
+            }
+        })
+        setPosts(postsFromAPI.data);
+    };
+
+    const publishPost = async () => {
+        await axios.post('https://backend-posts-test.herokuapp.com/api/post', {
+            title: postTile,
+            content: postContent
+        }, {
+            headers: {
+                'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImZyYW5jby5tb3JhbGVzQG91dGxvb2suY29tIiwic3ViIjo0LCJpYXQiOjE2NTgxOTc1ODksImV4cCI6MTY1ODIwMTE4OX0.wKyxRQJcqxVDfbjO_U4EsvYfOc2lX-zuij6bFc46MjM'
+            }
+        })
+    }
 
     return (
         <div>
@@ -21,6 +45,30 @@ const Sample = () => {
                 localStorage.setItem('userName', userName);
                 localStorage.setItem('contador', contador);
             }} />
+            <hr />
+            <input type="button" value="Obtener posts" onClick={ () => {
+                getPosts();
+            } } />
+            <h3>Agregar nuevo post</h3>
+            <input type="text" placeholder="Titulo post" onChange={(e) => setPostTile(e.target.value)} />
+            <input type="text" placeholder="Contenido post" onChange={(e) => setPostContent(e.target.value)} />
+            <input type="button" value="Publicar post" onClick={() => {
+                publishPost();
+            }} />
+            <table>
+                <tr>
+                    <th>Id</th>
+                    <th>Title</th>
+                    <th>Content</th>
+                </tr>
+                {posts.map((item) => (
+                    <tr key={item.id}>
+                        <td>{item.id}</td>
+                        <td>{item.title}</td>
+                        <td>{item.content}</td>
+                    </tr>
+                ))}
+            </table>
         </div>
     );
 };
