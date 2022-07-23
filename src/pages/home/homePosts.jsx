1b3943b3bcc0cb294adc81cd06acf3c3966647e3
francolmc <franco.morales@outlook.com>
2022-07-23 18:12:31 -0400

    feat: like post

--- src/pages/home/homePosts.jsx
+++ src/pages/home/homePosts.jsx
@@ -1,23 +1,38 @@
 import { useState } from "react";
 import { useEffect } from "react";
-import { Panel } from "rsuite";
+import { Badge, Button, Panel } from "rsuite";
 import { ApiWithToken } from "../../config/api";
 
 const HomePosts = () => {
     const [posts, setPosts] = useState([]);
+    
+    const getPosts = async () => {
+        const data = await ApiWithToken.get('/api/post/all/with-count-likes');
+        setPosts(data.data);
+    };
+
     useEffect(() => {
-        const getPosts = async () => {
-                const data = await ApiWithToken.get('/api/post');
-            setPosts(data.data);
-        };
         getPosts();
     }, []);
+
+    const likePost = async (postId) => {
+        await ApiWithToken.put(`/api/like/post/${postId}/like`);
+        await getPosts();
+    }
+
     return (
         <>
             <h3>Publicaciones realizadas</h3>
             {posts.map(item => (
                 <Panel header={item.title} key={item.id} className='post-list' shaded>
-                    {item.content}
+                    <p>
+                        {item.content}
+                    </p>
+                    <div className="likes-post">
+                        <Badge content={item.countLikes || 0}>
+                            <Button onClick={() => likePost(item.id)}>Like</Button>
+                        </Badge>
+                    </div>
                 </Panel>
             ))}
         </>
